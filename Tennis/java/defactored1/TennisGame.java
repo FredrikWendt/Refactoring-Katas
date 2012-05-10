2647eae6cca3eea6e074c82033d80905fc263bd9
Fredrik Wendt <fredrik@wendt.se>
2012-05-10 22:14:47 +0200

    fixed failing tests in defactored1

--- Tennis/java/defactored1/TennisGame.java
+++ Tennis/java/defactored1/TennisGame.java
@@ -13,7 +13,7 @@ public class TennisGame {
 	}
 
 	public void wonPoint(String playerName) {
-		if (playerName == "player1")
+		if (player1Name.equals(playerName))
 			m_score1 += 1;
 		else
 			m_score2 += 1;
@@ -49,8 +49,8 @@ public class TennisGame {
 			int minusResult = m_score1-m_score2;
 			if (minusResult==1) score ="Advantage player1";
 			else if (minusResult ==-1) score ="Advantage player2";
-			else if (minusResult>=2) score = "Win for player1";
-			else score ="Win for player2";
+			else if (minusResult>=2) score = "Win for " + player1Name;
+			else score ="Win for " + player2Name;
 		}
 		else
 		{
